36db45a05db3b18e499538d24d5859a5e5b42fc4
[reaqa766] <[reaqa766@gmail.com]>
2022-08-13 19:05:31 -0400

    Creacion del Array de Jugadores venezolanos MLB

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState, useEffect } from "react";
+// import data from "./data";
+// import SingleQuestion from "./Question";
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [datosP, setDatosP] = useState([]);
+  const url =
+    "https://statsapi.mlb.com/api/v1/sports/1/players?season=2022&gameType=R";
+
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const datosPlayers = await response.json();
+    const players = Object.values(datosPlayers);
+    console.log(datosPlayers.people);
+    const jugadoresFiltrados = datosPlayers.people.filter(
+      (jugador) => jugador.birthCountry === "Venezuela"
+    );
+    setDatosP(jugadoresFiltrados);
+    console.log(jugadoresFiltrados);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  // .filter((jugador) => jugador.birthCountry === "Venezuela")
+
+  return (
+    <>
+      <h2>jugadores</h2>
+      {datosP.map((datoPlayer, index) => {
+        // console.log("Index", index);
+        // console.log("datosP", datosP[index]);
+        // console.log("index", index);
+        // if (index) {
+        return (
+          <div key={datosP[index]}>
+            <h3>{datosP[index].fullName}</h3>
+            <h3>{datosP[index].birthCity}</h3>
+            <h3>{datosP[index].id}</h3>
+          </div>
+        );
+        // }
+      })}
+    </>
+  );
 }
 
 export default App;
