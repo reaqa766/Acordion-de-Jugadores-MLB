d5c8ad39c16920bc54044ccd3580e1a33dd48b1c
[reaqa766] <[reaqa766@gmail.com]>
2022-08-15 09:33:47 -0400

    Detalles de muestra de Acordion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 // import data from "./data";
-// import SingleQuestion from "./Question";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+// import data from "./data";
+import SingleQuestion from "./Question";
 
 function App() {
   const [datosP, setDatosP] = useState([]);
+  const [showInfo, setShowInfo] = useState(false);
   // const url =
   //   "https://statsapi.mlb.com/api/v1/people?personIds=650402&season=2018&&gamePk=529410&hydrate=stats(type=gameLog)";
   const url =
@@ -13,36 +17,63 @@ function App() {
     const response = await fetch(url);
     const datosPlayers = await response.json();
     const players = Object.values(datosPlayers);
-    console.log(datosPlayers.people);
+    // console.log(datosPlayers.people[0].stats[0].splits[101].stat.avg);
     const jugadoresFiltrados = datosPlayers.people.filter(
       (jugador) => jugador.birthCountry === "Venezuela"
     );
     setDatosP(jugadoresFiltrados);
+    // const jugadorID = [];
+    // const JugadoresId = jugadoresFiltrados.forEach((ids) => {
+    //   jugadorID.push([ids.id]);
+    // });
+    // setDatosP(jugadorID);
+    // console.log(jugadorID);
+    // console.log("jugadorID", jugadorID[0]);
     console.log(jugadoresFiltrados);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
-  // .filter((jugador) => jugador.birthCountry === "Venezuela")
 
   return (
-    <>
-      <h2>jugadores</h2>
-      {datosP.map((datoPlayer, index) => {
-        return (
-          <div key={datosP[index]} className="container">
-            <div className="question">
-              <h2>{datosP[index].fullName}</h2>
-              {/* <h3>{index}</h3> */}
-              <h3>{datosP[index].birthCity}</h3>
-              <h3>{datosP[index].id}</h3>
-            </div>
-          </div>
-        );
-        // }
-      })}
-    </>
+    <div className="container">
+      <h1 className="names">jugadores venezolanos</h1>
+      <section className="info2">
+        {datosP.map((datoPlayer, index) => {
+          return (
+            // <SingleQuestion key={datosP[index]} {...datosP} />
+            // <div key={datosP[index]} className="container">
+            <article key={datosP[index].currentTeam} className="question">
+              <header>
+                <div className="question">
+                  <h2 className="names">{datosP[index].fullName}</h2>
+                  <button
+                    className="btn"
+                    onClick={() => setShowInfo(!showInfo)}
+                  >
+                    {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                  </button>
+                </div>
+              </header>
+              <div className="info">
+                {showInfo && (
+                  <div>
+                    <h4>Fecha de Debut en MLB: {datosP[index].mlbDebutDate}</h4>
+                    <h4>Lugar de Nacimiento:{datosP[index].birthCity}</h4>
+                    <h4>Fecha de nacimiento: {datosP[index].birthDate}</h4>
+                    <h4>Edad: {datosP[index].currentAge}</h4>
+                    <h4>Codigo Interno MLB.com: {datosP[index].id}</h4>
+                  </div>
+                )}
+              </div>
+            </article>
+            // </div>
+          );
+        })}
+        ;
+      </section>
+    </div>
   );
 }
 
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,16 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const Question = () => {
-  return <h2>question component</h2>;
+  return (
+    <div></div>
+    // <div key={datosP[index]} className="container">
+    //   <div className="question">
+    //     <h2>{datosP[index].fullName}</h2>
+    //     <h3>{datosP[index].birthCity}</h3>
+    //     <h3>{datosP[index].id}</h3>
+    //   </div>
+    // </div>
+  );
 };
 
 export default Question;
